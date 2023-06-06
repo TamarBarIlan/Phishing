1569c44e967d3df67ad1227f712d719a4d567461
TamarBarIlan <tamarbarilan.4@gmail.com>
2023-06-06 18:41:12 +0300

    adding method

--- Phishing.py
+++ Phishing.py
@@ -3,17 +3,14 @@ from getpass import getpass
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-sender_email = "tam1362001@outlook.com"
-receiver_email = "tamarbarilan.4@gmail.com"
-password = 'tam123456'  # Please do not expose your password in the script. Use getpass to ask it when needed
+def send_email(sender_email, receiver_email, password, username, mail_service_name, title, job_title, personal_status, kids_status):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Hello " + username
+    message["From"] = sender_email
+    message["To"] = receiver_email
 
-message = MIMEMultipart("alternative")
-message["Subject"] = "Hello"
-message["From"] = sender_email
-message["To"] = receiver_email
-
-# Create the plain-text and HTML version of your message
-html = """\
+    # Create the HTML version of your message
+    html = """\
 <html>
   <head>
     <meta charset="utf-8">
@@ -86,15 +83,18 @@ html = """\
 </html>
 """
 
-# Turn these into plain/html MIMEText objects
-part1 = MIMEText(html, "html")
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(html, "html")
 
-# Add HTML/plain-text parts to MIMEMultipart message
-message.attach(part1)
+    # Add HTML/plain-text parts to MIMEMultipart message
+    message.attach(part1)
 
-# Create secure connection with server and send email
-context = ssl.create_default_context()
-with smtplib.SMTP("smtp.office365.com", 587) as server:
-    server.starttls(context=context)
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message.as_string())
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP("smtp.office365.com", 587) as server:
+        server.starttls(context=context)
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+# Usage:
+send_email("tam1362001@outlook.com", "tamarbarilan.4@gmail.com", 'tam123456', 'Username', 'Mail Service Name', 'Title', 'Job Title', 'Personal Status', 'Kids Status')
